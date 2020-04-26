cb3ef359a5167a61ab9fbaa6c2dcdf5ac0f1829c
Antonio Espinoza <aespinoza777@gmail.com>
2020-04-26 19:07:07 +0000

    Automatically backed up by Learn

--- lib/birthday.rb
+++ lib/birthday.rb
@@ -10,10 +10,4 @@ def happy_birthday(birthday_kids)
   end
 end
 
-def age_appropriate_birthday(birthday_kids)
-  birthday_kids.each do |kids_name, age|
-    if birthday_kids
-    puts "Happy Birthday #{kids_name}! You are now #{age} years old!"
-  end
-end
-end
+
